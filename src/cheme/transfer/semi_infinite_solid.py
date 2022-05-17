c19fc90019e25d4b150fadc566e37e3459ede2ed
noahgill409 <noahgill409@gmail.com>
2022-05-17 06:28:51 -0500

    find time and find temperature w/ tests

--- src/cheme/transfer/semi_infinite_solid.py
+++ src/cheme/transfer/semi_infinite_solid.py
@@ -15,13 +15,13 @@ from scipy.optimize import fsolve
 
 def wumbo(
     T: float,
-    time: float,
+    t: float,
     T0: float,
     T1: float,
-    h: float,
+    h: float | int,
     x: float,
     alpha: float,
-    k: float
+    k: float | int
 ) -> float:
     """unsteady-state conduction equation in a semi-infinite solid
 
@@ -61,40 +61,53 @@ def wumbo(
     fraction_of_change = (T - T0) / (T1 - T0)
 
     # shorthand parameters
-    m = math.sqrt(alpha*time)
+    m = math.sqrt(alpha*t)
     x_2m = x / (2 * m)
     hm_k = h * m / k
-    _term0 = hm_k * (x / m + hm_k)
+    exp_term = hm_k * (x / m + hm_k)
 
     zero = -1 * fraction_of_change + math.erfc(x_2m) - \
-        math.exp(_term0) * math.erfc(x_2m + hm_k)
+        math.exp(exp_term) * math.erfc(x_2m + hm_k)
 
     return zero
 
 
-def semi_infinite_solid(
-    time: float,
+def solve_for_T(*args, **kwargs) -> float:
+    return wumbo(*args, **kwargs)
+
+
+def solve_for_t(
+    t: float,
+    T: float,
+    T0: float,
+    T1: float,
+    h: float | int,
+    x: float,
+    alpha: float,
+    k: float | int
+) -> float:
+    return wumbo(T, t, T0, T1, h, x, alpha, k)
+
+
+def find_temperature(
+    t: float,
     T0: float,
     T1: float,
     h: float,
     x: float,
     alpha: float,
-    k: float
+    k: float,
+    initial_guess: float | None = None,
 ) -> float:
     """unsteady-state conduction equation in a semi-infinite solid
 
-    See Geankoplis 5.3B for a full explanation. Originally, the temperature in
-    the solid is uniform at T0. At time t = 0, the solid is suddenly exposed
-    to or immersed in a large mass of ambient fluid at tmemperature T1, which
-    is constant. The convection coefficient h in W/m^2*K or btu/h*ft^2*degF is
-    present and is constant; i.e., a surface resistance is present. Hence, the
-    temperature TS at the surface is not the same as T1.
+    See Geankoplis 5.3B for a full explanation.
 
 
     Parameters
     ----------
-    time : float
-        time in seconds
+    t : float
+        The time in seconds
     T0 : float
         The original uniform temperature of the solid in Kelvin
     T1 : float
@@ -111,13 +124,60 @@ def semi_infinite_solid(
     Returns
     -------
     float
-        temperature
+        time in seconds
     """
 
-    _T0 = (T0 + T1) / 2
-    _T0 = 267.97
+    if initial_guess is None:
+        initial_guess = (T0 + T1) / 2
 
-    soln = fsolve(wumbo, _T0, (time, T0, T1, h, x, alpha, k))
+    soln = fsolve(solve_for_T, initial_guess, (t, T0, T1, h, x, alpha, k))
+
+    assert isinstance(soln, np.ndarray)
+    return soln[0]
+
+
+def find_time(
+    T: float,
+    T0: float,
+    T1: float,
+    h: float,
+    x: float,
+    alpha: float,
+    k: float,
+    initial_guess: float | None = None
+) -> float:
+    """unsteady-state conduction equation in a semi-infinite solid
+
+    See Geankoplis 5.3B for a full explanation.
+
+
+    Parameters
+    ----------
+    T : float
+        The temperature at the specified point in Kelvin
+    T0 : float
+        The original uniform temperature of the solid in Kelvin
+    T1 : float
+        The temperature of the ambient fluid in Kelvin
+    h : float
+        convective coefficient in W/m^2*K
+    x : float
+        the distance into the solid from the surface in SI units in meters
+    alpha : float
+        solid property: alpha = k/rho*c_p in m^2/s.
+    k : float
+        solid property
+
+    Returns
+    -------
+    float
+        time in seconds
+    """
+
+    if initial_guess is None:
+        initial_guess = (k / h) ** 2 / alpha
+
+    soln = fsolve(solve_for_t, initial_guess, (T, T0, T1, h, x, alpha, k))
 
     assert isinstance(soln, np.ndarray)
     return soln[0]
--- tests/test_semi_infinite_solid.py
+++ tests/test_semi_infinite_solid.py
@@ -4,24 +4,24 @@
 # email:    noahgill409@gmail.com                                             #
 # package:  cheme                                                             #
 ###############################################################################
-
-Geankoplis example 5.3-1
-
-The depth in the soil of the earth at which freezing temperatures penetrate is
-often of importance in agriculture and construction. During a certain fall
-day, the temperature in the earth is a constant at 15.6 degC to a depth of
-several meters. A cold wave suddenly reduces the air temperature from 15.6 to
--17.8. The convective coefficient above the soil is 11.36 W/m^2*K. The soil
-properties can be assumed as alpha = 4.65e-7 m^2/s and k = 0.865 W/m*K.
-Neglect any latent heat effects.
 """
 
 import math
 
-from cheme.transfer.semi_infinite_solid import semi_infinite_solid
+from cheme.transfer.semi_infinite_solid import find_temperature, find_time
 
 
-def test_semi_infinite_solid():
+def test_solve_for_T():
+    """Geankoplis example 5.3-1
+
+    The depth in the soil of the earth at which freezing temperatures penetrate
+    is often of importance in agriculture and construction. During a certain
+    fall day, the temperature in the earth is a constant at 15.6 degC to a
+    depth of several meters. A cold wave suddenly reduces the air temperature
+    from 15.6 to -17.8. The convective coefficient above the soil is 11.36
+    W/m^2*K. The soil properties can be assumed as alpha = 4.65e-7 m^2/s and
+    k = 0.865 W/m*K. Neglect any latent heat effects.
+    """
     T0 = 15.6 + 273.15      # K
     T1 = -17.8 + 273.15     # K
     h = 11.36               # W / m^2 * K
@@ -29,9 +29,26 @@ def test_semi_infinite_solid():
     k = 0.865               # W / m * K
 
     # part a
-    time = 5 * 3600         # seconds
+    t = 5 * 3600         # seconds
     x = 0                   # m (surface)
 
-    T = semi_infinite_solid(time, T0, T1, h, x, alpha, k)
+    T = find_temperature(t, T0, T1, h, x, alpha, k)
+    assert math.fabs(T - 267.982) < 1e-3
 
-    assert math.fabs(T - 267.982) < 0.001
+
+def test_solve_for_t():
+    """Geankoplis problems 5.3-3: Cooling a slab of aluminum"""
+
+    T0 = 505.4      # K
+    T1 = 338.8      # K
+    T = 388.8       # K
+    x = 25.4E-3     # m
+
+    h = 455         # W / m^2 * K
+    alpha = 0.340   # m^2 / h
+    k = 208         # W / m * K
+
+    #
+
+    t = find_time(T, T0, T1, h, x, alpha, k)
+    assert math.fabs(t - 1.892) < 1e-3
